873c7fcf48805a5091665ab6d5c338a16cca34ae
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-10-13 23:17:56 -0400

    booked it through the challenge to turn something in by the due date, sorry for no other commits

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,43 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content: content });
+  const result = await request;
+
+  console.log('IndexDB has been updated', result)
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+
+  console.log(result);
+  if (result.length < 1) {
+    return false;
+  }
+  return result;
+};
+
+initdb();
